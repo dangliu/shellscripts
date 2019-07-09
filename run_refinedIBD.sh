bd5a8bd8f1299ace8848ca7b9f95b24661f9e16c
dangliu <d901809d@gmail.com>
2019-07-09 15:05:59 +0200

    modify refinedIBD running shell script

--- run_refinedIBD.sh
+++ run_refinedIBD.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-for run in {1..3}; do
 for chromosome in {1..22}; do
-java -Xss5m  -Xmx4g -jar ~/bin/BEAGLE/refined-ibd.jar gt=RUN${run}_Beagle5Phased${chromosome}.vcf.gz map=referenceFromMyData_Chr${chromosome}.map out=RefinedIBD_RUN${run}.$chromosome
-done
+java -Xss5m  -Xmx4g -jar ~/bin/BEAGLE/refined-ibd.jar gt=shapeitPhased${chromosome}.vcf map=chr${chromosome}.map out=RefinedIBD_RUN1.$chromosome
 done
